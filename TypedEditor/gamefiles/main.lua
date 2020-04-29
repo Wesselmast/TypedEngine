d5f4a36625c4224d8d3ff9996b16d73585400f7f
wessel mast <mastwessel@gmail.com>
2020-04-29 19:30:31 +0200

    Lua compile example

--- TypedEditor/gamefiles/main.lua
+++ TypedEditor/gamefiles/main.lua
@@ -1,17 +1,17 @@
 require "TEcore"
 
-te = TEcore;
+TE = TEcore;
 
-transform = te.TETransform()
-t = te.TESprite()
+transform = TE.TETransform()
+sprite = TE.TESprite()
 
 function begin()
    transform.position.x = 150
    transform.rotation = 0
-   t:init(transform, "res/textures/T_Tree.png")
+   sprite:init(transform, "res/textures/T_Tree.png")
 end
 
 function tick(deltaTime, time)
    transform.position.x = transform.position.x + (600 * deltaTime)
-   t:setTransform(transform)
+   sprite:setTransform(transform)
 end
