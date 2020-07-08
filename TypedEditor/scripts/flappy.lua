046f7cab730cdd1e5135ba30d93d3f60f73fb664
wessel mast <mastwessel@gmail.com>
2020-07-09 01:34:45 +0200

    Bouncing DVD demo

--- TypedEditor/scripts/flappy.lua
+++ TypedEditor/scripts/flappy.lua
@@ -1,14 +1,19 @@
 require 'TEcore'
 
 player = TEcore.TESprite()
+input = TEcore.TETransform()
+transform = TEcore.TETransform()
 
 function begin()
-   player:init("res/textures/T_Brick.jpg");
+   player:init("res/textures/T_DVD.png");
+   transform.scale.x = 0.25
+   transform.scale.y = 0.25
 end
 
 function tick(deltaTime, time)
-   local transform = TEcore.TETransform()
 
+   transform.position.x = transform.position.x + (input.position.x * 200 * deltaTime)
+   transform.position.y = transform.position.y + (input.position.y * 200 * deltaTime)
    -- if TEcore.isKeyDown(TEcore.W) then
    --    transform.position.y = transform.position.y + (200 * deltaTime)
    -- end
@@ -25,7 +30,16 @@ function tick(deltaTime, time)
 end
 
 function key_down(key)
-   if key == "T" then
-      print("i did it")
+   if key == "W" then 
+      input.position.y =  1 
+   elseif key == "A" then 
+      input.position.x = -1 
+   elseif key == "S" then 
+      input.position.y = -1 
+   elseif key == "D" then 
+      input.position.x =  1 
+   else
+      input.position.x =  0
+      input.position.y =  0
    end
 end
