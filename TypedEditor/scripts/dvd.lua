ac9b209139014870aa1beaafb61319732134c775
wessel mast <mastwessel@gmail.com>
2020-07-11 00:16:46 +0200

    Better dvd demo

--- TypedEditor/scripts/dvd.lua
+++ TypedEditor/scripts/dvd.lua
@@ -1,30 +1,30 @@
 require 'TEcore'
 
 player = TEcore.TESprite()
-input = TEcore.TETransform()
+input = TEcore.vec2()
 transform = TEcore.TETransform()
 
 function begin()
    player:init("res/textures/T_DVD.png");
    transform.scale.x = 0.25
    transform.scale.y = 0.25
+   input.x = 1
+   input.y = 1
 end
 
 function tick(deltaTime)
-   transform.position.x = transform.position.x + (input.position.x * 200 * deltaTime)
-   transform.position.y = transform.position.y + (input.position.y * 200 * deltaTime)
+   transform.position.x = transform.position.x + (input.x * 200 * deltaTime)
+   transform.position.y = transform.position.y + (input.y * 200 * deltaTime)
+
+   if transform.position.y < -500 then
+      input.y =  1 
+   elseif transform.position.y > 330 then
+      input.y = -1 
+   elseif transform.position.x < -700 then
+      input.x =  1 
+   elseif transform.position.x > 390 then
+      input.x = -1 
+   end
 
    player:setTransform(transform)
-end
-
-function key_down(key)
-   if key == "W" then 
-      input.position.y =  1 
-   elseif key == "A" then 
-      input.position.x = -1 
-   elseif key == "S" then 
-      input.position.y = -1 
-   elseif key == "D" then 
-      input.position.x =  1 
-   end
 end
